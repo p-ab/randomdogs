74e3be65c6a6207aba48da913f4998c81cb3a98c
Павел Абрамов <pasha.abramov.92@gmail.com>
2019-02-18 14:02:55 +1100

    stylefixing 6

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,7 +10,7 @@ import '../App.css';
 class App extends Component {
   render () {
     return (
-      <Router>      
+      <Router basename={process.env.PUBLIC_URL}>      
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
