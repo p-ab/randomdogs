265176f6a2a1355d6188546abc57e41d60244ec8
Павел Абрамов <pasha.abramov.92@gmail.com>
2019-02-18 13:15:15 +1100

    Updates

---
+++ precache-manifest.c1cde8d8a1a0f456a46b310152916c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f5790a39c8767dea01485df2717b108",
+    "url": "/randomdogs/static/media/logo.6f5790a3.png"
+  },
+  {
+    "revision": "5f4c3cd5ef35d03b93aa",
+    "url": "/randomdogs/static/js/runtime~main.5f4c3cd5.js"
+  },
+  {
+    "revision": "ac780c5683639585d220",
+    "url": "/randomdogs/static/js/main.ac780c56.chunk.js"
+  },
+  {
+    "revision": "8b30a88f8537708cfba0",
+    "url": "/randomdogs/static/js/2.8b30a88f.chunk.js"
+  },
+  {
+    "revision": "ac780c5683639585d220",
+    "url": "/randomdogs/static/css/main.b8efbbdf.chunk.css"
+  },
+  {
+    "revision": "3619a9b0ff553a19c7b91532cac25c98",
+    "url": "/randomdogs/index.html"
+  }
+];
